00a0fb155b531b2721b8828b7000ddcacab65eb9
Svartakatten <rikexhx@gmail.com>
2023-10-01 11:55:31 +0200

    Fixade kommentarer

--- Program.cs
+++ Program.cs
@@ -9,16 +9,17 @@ namespace NumbersGame
         static void Main(string[] args)
         {
 
+            //variablar om hur många försök och vad svar numret kommer att vara
             int tries = 0;
-            bool a = true;
             int number = 0;
 
+            //Spelets start, börjar med att fråga efter en svårighetsgrad och utgår därifrån
             while (true)
             {
                 Console.WriteLine("Välkommen till gissa numret! Vilken svårighetsgrad vill du ha mellan 1, 2 och 3");
 
                 int svårighetsgrad = int.Parse(Console.ReadLine());
-
+                //Dessa får användaren välja mellan om de vill ha svårare eller lättare för sig
                 if (svårighetsgrad == 1)
                 {
                     tries = 6;
@@ -38,13 +39,15 @@ namespace NumbersGame
                     number = random.Next(1, 30);
                 }
 
-
+                
                 Console.WriteLine($"Jag tänker på ett nummer. Kan du gissa vilket? Du får {tries} försök.");
 
+                //Här kollar programmet om svaret är korrekt eller inte
                 CheckGuess(tries, number, svårighetsgrad);
 
                 Console.WriteLine("Vill du spela igen? Y/N");
 
+                //Här kollar programmet om du vill forsätta spelet eller inte
                 string svar = Console.ReadLine();
                 if (svar == "N")
                 {
@@ -62,7 +65,7 @@ namespace NumbersGame
         }
         static void CheckGuess(int tries, int number, int svårighetsgrad)
         {
-
+            //Här skapas intar för nummer omkring så att jag kan ge andra texter om andvändaren kommer närmre svaret
             int close = number + 3;
             int close2 = close - 1;
             int close3 = close - 2;
@@ -70,11 +73,13 @@ namespace NumbersGame
             int close5 = close4 + 1;
             int close6 = close4 + 2;
 
+            //For Loop för att den ska fortsätta fråga efter svaret beroende på hur många försök du har kvar
             for (int i = 0; i < tries; i++)
             {
 
                 int definedanswer = int.Parse(Console.ReadLine());
 
+                //Här skapas lite arrayer för olika texter som sedan ska randomly skickas ut
                 string[] output = { "Tyvärr, du gissade för högt!", $"Haha! du gissade {definedanswer} och det var för högt", "Bra gissat men det var för högt!" };
                 string[] output2 = { "Tyvärr, du gissade för lågt!", $"Haha! du gissade {definedanswer} och det var för lägre", "Ganska bra gissat men det var för lågt!" };
 
@@ -82,6 +87,7 @@ namespace NumbersGame
 
                 int index = fel.Next(output.Length);
 
+                //Här kollar programmet om svaret är rätt eller rätt så nära
                 if (definedanswer == number)
                 {
                     Console.WriteLine("Wohoo! Du klarade det!");
@@ -123,7 +129,7 @@ namespace NumbersGame
                 }
 
             }
-
+            //Här avslutas spelet
             Console.WriteLine("Bra spelat!");
         }
     }
